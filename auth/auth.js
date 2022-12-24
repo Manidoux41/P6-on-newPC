37881a29793a36c9196d612e8d200a8342eb9d82
Manidoux41 <lherissondu41@gmail.com>
2022-12-24 14:55:43 +0100

    authentication login ok

--- auth/auth.js
+++ auth/auth.js
@@ -14,7 +14,34 @@ passport.use(
             const user = await UserModel.create({ email, password })
             return done(null, user)
         } catch (error) {
-            done(error)
+            return done(error)
+        }
+    })
+)
+
+passport.use(
+    'login',
+    new Strategy({
+        usernameField : 'email',
+        passwordField : 'password'
+    },
+    async (email, password, done) => {
+        try {
+            const user = await UserModel.findOne({email})
+
+            if (!user) {
+                return done(null, false, { message: 'User not found'})
+            }
+
+            const validate = await user.isValidPassword(password);
+
+            if(!validate) {
+                return done(null, false, { message: 'Mot de passe/username incorrect'})
+            }
+
+            return done(null, user, { message: 'Connexion successfull !'})
+        } catch (error) {
+            return done(error)
         }
     })
 )
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -14,5 +14,25 @@ userRouter.post('/signup', passport.authenticate('signup', { session: false }),
     }
 )
 
+userRouter.post('/login', (req,res,next) => {
+    passport.authenticate('login', async (err,user) => {
+        try {
+            if(err || !user) {
+                const error = new Error('Une erreur est survenue.')
+                return next(error)
+            }
+            req.login(user, { session: false}, async error => {
+                if(error) return next(error)
+                const body = {id: user._id, email: user.email}
+                res.json(body)
+            })
+        } catch (error) {
+            return next(error)
+        }
+    })(req,res,next)
+})
+
+
+
 
 export default userRouter;
